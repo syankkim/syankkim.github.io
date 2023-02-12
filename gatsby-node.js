61ddb9323abcda1fbec8b1d5f6c4852ab3945645
syankkim <ksso7300@gmail.com>
2023-02-12 14:47:28 +0900

    gatsyby blog 환경설정

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,9 +10,27 @@
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
+    path: '/using-dsg',
+    component: require.resolve('./src/templates/using-dsg.js'),
     context: {},
     defer: true,
+  })
+}
+
+const path = require('path')
+
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {}
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        // components, utils, hooks로 시작하는 경로는 모두 src 폴더 내의 components 폴더로 매핑해주어 절대 경로를 사용가능
+        components: path.resolve(__dirname, 'src/components'),
+        utils: path.resolve(__dirname, 'src/utils'),
+        hooks: path.resolve(__dirname, 'src/hooks'),
+      },
+    },
   })
 }
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from 'react'
+import Text from 'components/Text'
+
+// FunctionComponent: 리액트에서의 기본 함수형 컴포넌트 타입
+const IndexPage: FunctionComponent = function () {
+  return <Text text="Home" />
+}
+
+export default IndexPage
