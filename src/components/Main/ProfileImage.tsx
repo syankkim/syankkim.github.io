f86b31543f32672e4f8d2b2a748a12d6b1d14b70
syankkim <ksso7300@gmail.com>
2023-02-12 19:09:56 +0900

    프로필 이미지 추가

---
+++ src/components/Main/ProfileImage.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+
+const PROFILE_IMAGE_LINK =
+  'https://user-images.githubusercontent.com/28856435/218304344-0f1ab181-9d8b-4774-a01b-d22958637c5f.png'
+
+const ProfileImageWrapper = styled.img`
+  width: 200px;
+  height: 200px;
+  margin-bottom: 30px;
+  border-radius: 50%;
+  background-color: grey;
+`
+const ProfileImage: FunctionComponent = function () {
+  return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
+}
+
+export default ProfileImage
