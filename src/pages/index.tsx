20050466f7934d05adddc7e941166244470d6bd5
syankkim <ksso7300@gmail.com>
2023-02-12 15:13:15 +0900

    Text 컴포넌트 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import React, { FunctionComponent } from 'react'
-import Text from 'components/Text'
+import { Link } from 'gatsby'
 
 // FunctionComponent: 리액트에서의 기본 함수형 컴포넌트 타입
 const IndexPage: FunctionComponent = function () {
-  return <Text text="Home" />
+  return (
+    <div>
+      <Link to="/info/">To Info</Link>
+    </div>
+  )
 }
 
 export default IndexPage
